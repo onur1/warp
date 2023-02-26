39666a0d835624499fa7c90f09ddebf3e7cde2e0
onur1 <onur1@onurgunduz.com>
2023-02-26 18:43:52 +0100

    Add package doc

--- types.go
+++ types.go
@@ -1,3 +1,6 @@
+// Package data provides a set of useful data types for working with different
+// types of data, such as data that contains an error, data that doesn't exist,
+// data that doesn't exist yet, data that keeps changing, and etc.
 package data
 
 import (
